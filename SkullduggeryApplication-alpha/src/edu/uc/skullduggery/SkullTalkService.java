58890703819a920ffe124aa608b1e24e5f79e490
rkoeninger <rkoeninger@att.net>
2010-03-26 17:28:44 +0000

    git-svn-id: http://skullduggery.googlecode.com/svn/trunk@54 edacab16-c986-11de-9e96-3193d784f102

--- SkullduggeryApplication-alpha/src/edu/uc/skullduggery/SkullTalkService.java
+++ SkullduggeryApplication-alpha/src/edu/uc/skullduggery/SkullTalkService.java
@@ -1,15 +1,86 @@
 package edu.uc.skullduggery;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
+import java.math.BigInteger;
+import android.os.Handler;
+import android.os.Message;
 
-public class SkullTalkService extends Service {
+/**
+ * This class is a collection of threads that constitute the process
+ * of an encrypted conversation and the data related to it.
+ * 
+ * A single instance should be all that is necessary for as long as the
+ * app runs.
+ */
+public class SkullTalkService{
+	
+	public static enum CallState
+	{LISTENING, CALLING, TALKING};
+	
+	private BigInteger transmitKey;
+	private BigInteger receiveKey;
+	private CallState callState;
+	private Thread callThread;
+	private Thread acceptThread;
+	private Thread transmitThread;
+	private Thread receiveThread;
+	private Handler uiHandler;
+	
+	public SkullTalkService(Handler uiHandler){
+		this.uiHandler = uiHandler;
+	}
+	
+	public void start(){
+		// Contact server to register ip info
+		listen();
+	}
+	
+	public void hangup(){
+		endComm();
+	}
+	
+	/*
+	 * Functions are synchronized so there won't be concurrent changes
+	 * to the state of the service from the UI/TransmitThread/ReceiveThread
+	 */
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
-		return null;
+	public synchronized void dial(String number){
+		callState = CallState.CALLING;//stops accept thread
+		acceptThread = null;
+		callThread = new CallThread(number);
+		callThread.start();
+	}
+	
+	//gets called initially by start() or constructor
+	private synchronized void listen(){
+		callState = CallState.LISTENING;
+		acceptThread = new AcceptThread();
+		acceptThread.start();
+	}
+
+	//called from callThread or acceptThread once handshake
+	// is completed
+	private synchronized void startComm(){
+		callState = CallState.TALKING;
+		callThread = null;
+		acceptThread = null;
+		transmitThread = new TransmitThread();
+		receiveThread = new ReceiveThread();
+		transmitThread.start();
+		receiveThread.start();
+		uiHandler.handleMessage(
+		Message.obtain(uiHandler, 1));
+	}
+	
+	// called if error reported by receiveThread or transmitThread
+	// or if callThread fails.
+	// OR if user hangs-up.
+	private synchronized void endComm(){
+		callState = CallState.LISTENING;
+		transmitThread = null;
+		receiveThread = null;
+		uiHandler.handleMessage(
+		Message.obtain(uiHandler, 2));
+		listen();
 	}
 	
 	/**
@@ -22,10 +93,35 @@ public class SkullTalkService extends Service {
 	 * Stopped by itself when the connection to the other phone
 	 * has been established or the recipient is unreachable or if the
 	 * user hangs-up prematurely.
-	 * 
-	 * @author bort
 	 */
 	public class CallThread extends Thread{
+		private String number;
+		public CallThread(String number){
+			this.number = number;
+		}
+		public void run(){
+			// Opens port to listener on receiving phone/
+			//                      request direction from switch station
+			
+			// Open a socket between the phones
+			
+			// Write first packet including our encrypt key
+			
+			// Read response packet from receiving phone
+			
+			// ^^^This is the handshake protocol
+			
+			/* Start transmit thread
+			 * Start receive thread
+			 * Notify Activity that call has started through a callback,
+			 *     so it can update the UI
+			 * End this thread
+			 * Any running AcceptThread should stop also
+			 * 
+			 * ^^^ This is all done by the call below
+			 */
+			SkullTalkService.this.startComm();
+		}
 	}
 	
 	/**
@@ -37,10 +133,34 @@ public class SkullTalkService extends Service {
 	 * has ended.
 	 * 
 	 * Stopped once a call has started.
-	 * 
-	 * @author bort
 	 */
 	public class AcceptThread extends Thread{
+		public void run(){
+			// Receives call (srvskt.accept();)/
+			//               reads call after polling switch station
+			
+			//At this point, a socket is opened between the phones
+			
+			//Read first packet from calling phone which includes
+			//encrypt key
+			
+			//Send a packet which includes this phone's public key
+			
+			//^^^This is the handshake procedure
+			
+			
+			/* Start transmit thread
+			 * Start receive thread
+			 * Notify Activity that call has started through a callback,
+			 *     so it can update the UI
+			 * End this thread
+			 * 
+			 * ^^^ This is all done by the call below
+			 */
+			SkullTalkService.this.startComm();
+			
+			
+		}
 	}
 	
 	/**
@@ -52,10 +172,25 @@ public class SkullTalkService extends Service {
 	 * 
 	 * Stopped by SkullTalkService if user hangs up or
 	 * if ReceiveThread gets a hang-up signal.
-	 * 
-	 * @author bort
 	 */
 	public class TransmitThread extends Thread{
+		public void run(){
+			// check that the call is still in progress and socket is open
+			if (SkullTalkService.this.callState != CallState.TALKING){
+				return;
+			}
+			
+			// read a block of data from the audio buffer
+			
+			// perform block cipher
+			
+			// write voice data packet to other phone
+			
+			
+			//If at any point, call has ended or connect error,
+			//                  end this thread
+			SkullTalkService.this.endComm();
+		}
 	}
 	
 	/**
@@ -67,10 +202,25 @@ public class SkullTalkService extends Service {
 	 * 
 	 * Stopped by SkullTalkService if user hangs up or
 	 * if a hang-up signal is read from the other phone.
-	 * 
-	 * @author bort
 	 */
 	public class ReceiveThread extends Thread{
+		public void run(){
+			// check that the call is still in progress and socket is open
+			if (SkullTalkService.this.callState != CallState.TALKING){
+				return;
+			}
+			
+			// read a packet from the socket
+			
+			// perform block cipher
+			
+			// write voice data to audio buffer
+			
+			
+			//If at any point, call has ended or connect error,
+			//                  end this thread
+			SkullTalkService.this.endComm();
+		}
 	}
 
 }
--- SkullduggeryApplication-alpha/src/edu/uc/skullduggery/Skullduggery.java
+++ SkullduggeryApplication-alpha/src/edu/uc/skullduggery/Skullduggery.java
@@ -2,12 +2,95 @@ package edu.uc.skullduggery;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+import android.view.View;
 
 public class Skullduggery extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+	
+	private SkullTalkService service;
+	private final Handler handlerCallback = new Handler(){
+		public void handleMessage(Message msg){
+			switch (msg.what){
+			case 1:
+				//show talk screen
+			case 2:
+				//show dial screen
+			default:
+				//error
+			}
+		}
+	};
+	
+    View.OnClickListener callButtonListener = new View.OnClickListener(){
+        public void onClick(View view){
+        	String phoneNum =
+        	((TextView) findViewById(R.id.phoneNumField)).getText().toString();
+        	service.dial(phoneNum);
+        	// After service hangs-up, handler
+        	// is called and UI is updated there
+        }
+    };
+    
+    View.OnClickListener hangupButtonListener = new View.OnClickListener(){
+        public void onClick(View view){
+        	service.hangup();
+        	// After service hangs-up, handler
+        	// is called and UI is updated there
+        }
+    };
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        findViewById(R.id.callButton).setOnClickListener(callButtonListener);
+        findViewById(R.id.hangupButton).setOnClickListener(hangupButtonListener);
+        
+        service = new SkullTalkService(handlerCallback);
+        
     }
+    
+    public void onDestroy(){
+    	
+    	//stop call
+    	
+    	super.onDestroy();
+    	
+    }
+
+    public void onStart(){
+    	super.onStart();
+    	
+    }
+    
+    public void onStop(){
+    	
+    	//possibly stop call
+    	
+    	super.onStop();
+    	
+    }
+    
+    public void onResume(){
+    	super.onResume();
+    	
+    }
+    
+    public void onPause(){
+    	super.onPause();
+    	
+    }
+    
+    public void onSaveInstanceState(Bundle outState){
+    	super.onSaveInstanceState(outState);
+    	
+    }
+    
+    public void onRestoreInstanceState(Bundle savedInstanceState){
+    	super.onRestoreInstanceState(savedInstanceState);
+    	
+    }
+    
 }
