44f3311542de6b3f00e43823c16bc2cbf8dbfc07
bakergo <bakergo@gmail.com>
2010-03-30 06:28:11 +0000

    Skullduggery Audio Test -- Initial Commit. Recording, playback of voice, networking. To be used with Skullduggery AudioTestServer
    git-svn-id: http://skullduggery.googlecode.com/svn/trunk@59 edacab16-c986-11de-9e96-3193d784f102

---
+++ SkullduggeryAudioTestServer/src/edu/uc/skullduggery/SkullduggeryAudioTestServer.java
@@ -0,0 +1,88 @@
+package edu.uc.skullduggery;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SkullduggeryAudioTestServer {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	
+	private static void PlayFileToClients(File f, ServerSocket servSock) throws IOException
+	{
+		FileInputStream fin = new FileInputStream(f);
+		while(true){
+			Socket clientSock = servSock.accept();
+			byte[] dataBuffer = new byte[1024];
+			int dataLength;
+			DataOutputStream dataSock = new DataOutputStream(clientSock.getOutputStream());
+			while((dataLength = fin.read(dataBuffer)) > 0)
+			{
+				dataSock.write(dataBuffer, 0, dataLength);
+			}
+			dataSock.close();
+			clientSock.close();
+			fin.reset();
+		}
+	}
+	
+	private static void RecordFileFromClient(File f, ServerSocket servSock) throws IOException
+	{
+		FileOutputStream fout = new FileOutputStream(f);
+		Socket clientSock = servSock.accept();
+		byte[] dataBuffer = new byte[1024];
+		int dataLength;
+		DataInputStream dataSock = new DataInputStream(clientSock.getInputStream());
+		while((dataLength = dataSock.read(dataBuffer)) > 0)
+		{
+			fout.write(dataBuffer, 0 , dataLength);
+		}
+		fout.flush();
+		fout.close();
+		clientSock.close();
+	}
+	
+	public static void main(String[] args) {
+		if(args.length <= 1)
+		{
+			System.out.println("Usage: [PROG_NAME] [FILE]");
+			return;
+		}
+		File f = new File(args[1]);
+		
+		try{
+			ServerSocket s = new ServerSocket(9002);
+			if(!f.exists())
+			{
+				if(!f.canWrite())
+				{
+					System.out.println("Can't write the file. Exiting.");
+					return;
+				}
+				//create the file
+				RecordFileFromClient(f,s);
+			}
+			
+			if(!f.canRead())
+			{
+				System.out.println("Can't read the file. Exiting.");
+				return;
+			}
+			//Play the file
+			PlayFileToClients(f,s);
+			s.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
