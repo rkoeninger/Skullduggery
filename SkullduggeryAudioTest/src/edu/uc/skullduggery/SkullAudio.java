6c5b51ec204ade9b81289ce33fbcecd377594aa3
bakergo <bakergo@gmail.com>
2010-04-01 05:38:58 +0000

    Revised to fix bugs in transmission, etc. Much cleaner, with debug messages. Recording on the server works. Playback does not.
    git-svn-id: http://skullduggery.googlecode.com/svn/trunk@64 edacab16-c986-11de-9e96-3193d784f102

--- SkullduggeryAudioTest/src/edu/uc/skullduggery/SkullAudio.java
+++ SkullduggeryAudioTest/src/edu/uc/skullduggery/SkullAudio.java
@@ -6,119 +6,134 @@ import android.media.AudioManager;
 import android.media.AudioRecord;
 import android.media.AudioTrack;
 import android.media.MediaRecorder;
-import android.media.AudioRecord.OnRecordPositionUpdateListener;
 import android.os.Bundle;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.*;
 
 public class SkullAudio extends Activity {
 	private ListenThread T;
+	private SpeakThread S;
+	private Socket soundSock;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        T = new ListenThread();
-        T.start();
+        
+        try {
+			soundSock = new Socket("10.0.2.2", 9002);
+	        T = new ListenThread(soundSock);
+	        S = new SpeakThread(soundSock);
+	        T.start();
+	        S.start();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    private class SpeakThread extends Thread{
+    	private Socket soundSock;
+    	
+    	public SpeakThread(Socket S)
+    	{
+    		soundSock = S;
+    	}
+    	
+    	public void run()
+    	{
+    		try
+    		{
+    			android.util.Log.d("SkullAudio", "Beginning audio recording thread.");
+	    		int source = MediaRecorder.AudioSource.DEFAULT;
+	    		int bitRate = 8000; //11025, 22050, 44100
+	    		int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+	    		int encoding = AudioFormat.ENCODING_PCM_16BIT;
+	    		int bufSize = AudioRecord.getMinBufferSize(bitRate, channelConfig, encoding);
+	    		int len = 0;
+
+    			android.util.Log.d("SkullAudio", "Initializing output stream.");
+	    		DataOutputStream DOS = new DataOutputStream(soundSock.getOutputStream());
+	    		
+	    		DOS.writeInt(bitRate);
+	    		//DOS.writeInt(channelConfig);
+	    		DOS.writeInt(encoding);
+	    		
+	    		byte[] buffer = new byte[1024];
+
+    			android.util.Log.d("SkullAudio", "Initializing recorder.");
+	    		AudioRecord SoundRecorder = new AudioRecord(source, bitRate, channelConfig, encoding, bufSize);
+
+    			android.util.Log.d("SkullAudio", "Starting to record.");
+	    		SoundRecorder.startRecording();
+	
+	    		while(SoundRecorder.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING && soundSock.isConnected())
+	    		{
+	    			android.util.Log.d("SkullAudio", "Reading recorded data.");
+	        		while((len = SoundRecorder.read(buffer, 0, buffer.length)) > 0)
+	    				DOS.write(buffer, 0, len);
+
+	    			android.util.Log.d("SkullAudio", "Recorded data written.");
+	    		}
+
+    			android.util.Log.d("SkullAudio", "Connection closed, etc.");
+	    		
+	    		SoundRecorder.stop();
+    		}
+    		catch (IOException e)
+    		{
+    			android.util.Log.e("SkullAudio", "Error when recording audio.");
+    			android.util.Log.e("SkullAudio", e.getMessage());
+    		}
+    	}
     }
     
     
     private class ListenThread extends Thread{
     	
     	private Socket soundSock;
-
-    	public ListenThread()
+    	
+    	public ListenThread(Socket S)
     	{
-    		int source = MediaRecorder.AudioSource.DEFAULT;
-    		int bitRate = 8000; //11025, 22050, 44100
-    		int channelConfig = AudioFormat.CHANNEL_IN_MONO;
-    		int encoding = AudioFormat.ENCODING_PCM_16BIT;
-    		//int encoding = AudioFormat.ENCODING_PCM_8BIT;
-    		int bufSize = AudioRecord.getMinBufferSize(bitRate, channelConfig, encoding);
-    		
-    		AudioRecord SoundRecorder = new AudioRecord(source, bitRate, channelConfig, encoding, bufSize);
-            SoundRecorder.setRecordPositionUpdateListener(
-            		new OnRecordPositionUpdateListener() {    					
-    					@Override
-    					public void onPeriodicNotification(AudioRecord recorder) {
-    						android.util.Log.d("SkullAudio", "Periodic Listener enperioded.");
-    				
-    						byte[] audioData = new byte[1024];
-    						int len = 0;
-    						
-    						while((len = recorder.read(audioData, 0, audioData.length)) > 0)
-
-    						{
-    							try {
-    	    						android.util.Log.d("SkullAudio", "Read " + len + " bytes from audio.");
-
-    								write(audioData, len);
-    							} catch (IOException e) {
-    								e.printStackTrace();
-    								break;
-    							}
-    						}
-    						
-    						// TODO Auto-generated method stub
-    						
-    					}
-    					
-    					@Override
-    					public void onMarkerReached(AudioRecord recorder) {
-    						int format = 		recorder.getAudioFormat();
-    						int channelConfig = recorder.getChannelConfiguration();
-    						int sampleRate = 	recorder.getSampleRate();
-    						try {
-    							DataOutputStream DOS = new DataOutputStream(soundSock.getOutputStream());
-    							DOS.writeInt(sampleRate);
-    							DOS.writeInt(channelConfig);
-    							DOS.writeInt(format);
-    						}
-    						catch (IOException e) {
-    						
-    						}
-    						
-    						onPeriodicNotification(recorder);
-    					}
-    				});
-            SoundRecorder.setPositionNotificationPeriod(1);
-            SoundRecorder.startRecording();
-            
-    		try{
-    			soundSock = new Socket("10.0.2.2", 9002);
-    		}
-    		catch(IOException e)
-    		{
-    			//Print an error message, etc.
-    		}
+    		soundSock = S;
     	}
     	
     	public void run()
     	{
-    		DataInputStream rawAudio = null;
-    		try {
-    			rawAudio = new DataInputStream(soundSock.getInputStream());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			byte[] buf = new byte[1024];
-			
-			int sampleRate, channelConfig, format;
-			AudioTrack track;
-			
-			try{
+    		try{
+
+    			android.util.Log.d("SkullAudio", "Initializing the reading stream");
+    			DataInputStream rawAudio = new DataInputStream(soundSock.getInputStream());
+				
+				byte[] buf = new byte[1024];
+				int sampleRate, channelConfig, format;
+				AudioTrack track;
+				
 				if(soundSock.isConnected() && !soundSock.isClosed())
 				{
+
+					android.util.Log.d("SkullAudio", "Reading sample rate");
 					sampleRate = rawAudio.readInt();
-					channelConfig = rawAudio.readInt();
+					android.util.Log.d("SkullAudio", "Sample rate:" + Integer.toString(sampleRate));
+					
+					android.util.Log.d("SkullAudio", "Reading channel config");
+					channelConfig = AudioFormat.CHANNEL_OUT_MONO;
+//					channelConfig = rawAudio.readInt();
+					android.util.Log.d("SkullAudio", "Channel Config:" + Integer.toString(channelConfig));
+					
+					android.util.Log.d("SkullAudio", "Reading audio format");
 					format = rawAudio.readInt();
+					android.util.Log.d("SkullAudio", "Audio format:" + Integer.toString(format));
+					
+					android.util.Log.d("SkullAudio", "Initializing track");
 					track = new AudioTrack(AudioManager.USE_DEFAULT_STREAM_TYPE, sampleRate, channelConfig, format, AudioTrack.getMinBufferSize(sampleRate, channelConfig, format), AudioTrack.MODE_STREAM);
+
 				}
 				else
 				{
@@ -126,39 +141,30 @@ public class SkullAudio extends Activity {
 					soundSock.close();
 					return;
 				}
-			}
-			catch(IOException e)
-			{
-				return;
-			}
-
-			try {
+				android.util.Log.d("SkullAudio", "Track initialized. Playing back data.");
+				
 				while(soundSock.isConnected() && ! soundSock.isClosed())
 				{
+					android.util.Log.d("SkullAudio", "Beginning read");
+					
 					int bytesRead = rawAudio.read(buf);
 					track.write(buf, 0, bytesRead);
 					if(track.getPlayState() != AudioTrack.PLAYSTATE_PLAYING){
 						track.play();
 					}
+					android.util.Log.d("SkullAudio", "Written to buffer");
+					
 				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				try {
-					rawAudio.close();
-					soundSock.close();
-				}
-				catch (IOException e1) {
-					// TODO Auto-generated catch block
-				}
-			}
-    	}
-    	
-    	public void write(byte[] audioData, int length) throws IOException
-    	{
-//    		DataOutputStream rawAudio;
-    		OutputStream soundSockOutput = soundSock.getOutputStream();
-  //  		rawAudio = new DataOutputStream(soundSockOutput);
-    		soundSockOutput.write(audioData, 0, length);
+				android.util.Log.d("SkullAudio", "Done. Closing up.");
+				
+				rawAudio.close();
+				soundSock.close();
+    		}
+    		catch (IOException e)
+    		{
+    			android.util.Log.e("SkullAudio", "Error when playing audio.");
+    			android.util.Log.e("SkullAudio", e.getMessage());
+    		}
     	}
     }
 }
