1eea9bd2794f0f113c60f250f115a5d6a1afce1a
rkoeninger <rkoeninger@att.net>
2010-04-22 18:06:35 +0000

    git-svn-id: http://skullduggery.googlecode.com/svn/trunk@102 edacab16-c986-11de-9e96-3193d784f102

--- SkullduggeryApplication-alpha/src/edu/uc/skullduggery/Constants.java
+++ SkullduggeryApplication-alpha/src/edu/uc/skullduggery/Constants.java
@@ -1,5 +1,9 @@
 package edu.uc.skullduggery;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.StringTokenizer;
+
 public class Constants {
 	public static final String USERDBNAME = "SkullStorage";
 	public static final String USERINFOTABLE = "UserInfo";
@@ -29,5 +33,41 @@ public class Constants {
 	public static final int ASYMKEYSIZE = 4096;
 	public static final byte[] MAGICBYTES = "SKUL".getBytes();
 	
+	
+	public static int ipBytesToInt(byte[] ipBytes){
+		int ip32 = 0;
+		for (int x = 0; x < ipBytes.length; ++x){
+			ip32 |= (ipBytes[x] & 0xff) << ((ipBytes.length - 1 - x) * 8); 
+		}
+		return ip32;
+	}
+	public static byte[] ipIntToBytes(int ip32){
+		byte[] ipBytes = new byte[4];
+		for (int x = 0; x < ipBytes.length; ++x){
+			ipBytes[x] = (byte)
+			((ip32 >> ((ipBytes.length - 1 - x) * 8)) & 0xff);
+		}
+		return ipBytes;
+	}
+	public static byte[] ipStringToBytes(String ipString){
+		try{
+			return InetAddress.getByName(ipString).getAddress();
+		}catch (IOException ioe){
+			StringTokenizer tokens = new StringTokenizer(ipString);
+			byte[] ipBytes = new byte[4];
+			for (int x = 0; x < ipBytes.length; ++x){
+				ipBytes[x] = (byte) Integer.parseInt(tokens.nextToken());
+			}
+			return ipBytes;
+		}
+	}
+	public static String ipBytesToString(byte[] ipBytes){
+		try{
+			return InetAddress.getByAddress(ipBytes).getHostAddress();
+		}catch (IOException ioe){
+			return ((int) ipBytes[0]) + "." + ((int) ipBytes[1]) +
+			"." + ((int) ipBytes[2]) + "." + ((int) ipBytes[3]);
+		}
+	}
 	
 }
