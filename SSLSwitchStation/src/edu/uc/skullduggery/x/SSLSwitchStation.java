27758751b10b35defd821eee6d777d53c2e106ad
bakergo <bakergo@gmail.com>
2010-04-29 19:06:46 +0000

    + No Certificate available error. + Updated to use InetAddress/InetSocketAddress instead of int ip, etc. + Updated to be a bit cleaner.
    git-svn-id: http://skullduggery.googlecode.com/svn/trunk@120 edacab16-c986-11de-9e96-3193d784f102

--- SSLSwitchStation/src/edu/uc/skullduggery/x/SSLSwitchStation.java
+++ SSLSwitchStation/src/edu/uc/skullduggery/x/SSLSwitchStation.java
@@ -8,7 +8,27 @@ import java.util.*;
 import javax.net.ssl.*;
 
 public class SSLSwitchStation {
+	public static final String[] SSLCIPHERSUITES = 
+		new String[] {
+		"SSL_DH_anon_WITH_RC4_128_MD5"
+		,"TLS_DH_anon_WITH_AES_128_CBC_SHA"
+		,"TLS_DH_anon_WITH_AES_256_CBC_SHA"
+		,"SSL_DH_anon_WITH_3DES_EDE_CBC_SHA"
+		,"SSL_DH_anon_WITH_DES_CBC_SHA"
+		,"TLS_ECDH_anon_WITH_RC4_128_SHA"
+		,"TLS_ECDH_anon_WITH_AES_128_CBC_SHA"
+		,"TLS_ECDH_anon_WITH_AES_256_CBC_SHA"
+		,"TLS_ECDH_anon_WITH_3DES_EDE_CBC_SHA"
+		,"SSL_DH_anon_EXPORT_WITH_RC4_40_MD5"
+		,"SSL_DH_anon_EXPORT_WITH_DES40_CBC_SHA"
+		,"TLS_ECDH_anon_WITH_NULL_SHA"
+	};                       
+	
 	public static void main(String[] args){
+		if(args.length == 0) {
+			System.out.println("Usage: SSLSwitchStation PORTNUM");
+			System.exit(0);
+		}
 		int listenPort = Integer.parseInt(args[0]);
 		SSLSwitchStation server = new SSLSwitchStation(listenPort);
 		server.start();
@@ -17,12 +37,10 @@ public class SSLSwitchStation {
 	 * Simple struct to contain info about how to contact a phone.
 	 */
 	public static class PhoneInfo{
-		public int ip;
-		public int port;
+		public InetSocketAddress ip;
 		public long sessionKey;
-		public PhoneInfo(int ip, int port, long sessionKey){
-			this.ip = ip;
-			this.port = port;
+		public PhoneInfo(InetAddress ip, int port, long sessionKey){
+			this.ip = new InetSocketAddress(ip, port);
 			this.sessionKey = sessionKey;
 		}
 	}
@@ -53,6 +71,7 @@ public class SSLSwitchStation {
 			try{
 				acceptSocket = SSLServerSocketFactory.getDefault().
 				createServerSocket(listenPort);
+//				((SSLServerSocket) acceptSocket).setEnabledCipherSuites(SSLCIPHERSUITES);
 				acceptSocket.setSoTimeout(100);
 				while (! letDie){
 					try{
@@ -78,6 +97,154 @@ public class SSLSwitchStation {
 			this.connection = connection;
 			processThreads.add(this);
 		}
+		
+		
+		
+		private void register(DataInputStream input, DataOutputStream output) throws IOException
+		{
+			// Read phone number from socket
+			int phoneNumLength = input.readByte();
+			byte[] phoneNumBytes = new byte[phoneNumLength];
+			input.readFully(phoneNumBytes);
+			String phoneNum = new String(phoneNumBytes);
+			
+			// Read ip and port bytes from socket
+			InetAddress ip = connection.getInetAddress();
+			short port = input.readShort();
+			
+			long sessionKey = sessionKeyGen.nextLong();
+			
+			PhoneInfo info = new PhoneInfo(ip, port, sessionKey);
+			
+			if (phoneTable.containsValue(info)){
+				
+				output.writeByte(1);
+
+				// Logging
+				System.out.print("! Request FAIL ");
+				System.out.print(phoneNum);
+				System.out.println();
+				
+				return;
+				
+			}
+			
+			// Add value to table
+			phoneTable.put(phoneNum, info);
+			
+			// Indicate success
+			output.writeByte(0);
+			output.writeLong(sessionKey);
+			
+			// Logging
+			System.out.print("+ Phone info added ");
+			System.out.print(phoneNum);
+			System.out.print(',');
+			System.out.print(ip.toString());
+			System.out.print(':');
+			System.out.print(port);
+			System.out.println();	
+		}
+		
+		private void infoRequest(DataInputStream input, DataOutputStream output) throws IOException
+		{
+			// Read phone number from socket
+			int phoneNumLength = input.readByte();
+			byte[] phoneNumBytes = new byte[phoneNumLength];
+			input.readFully(phoneNumBytes);
+			String phoneNum = new String(phoneNumBytes);
+			
+			// Get phone info from table
+			PhoneInfo info = phoneTable.get(phoneNum);
+			
+			if (info == null){
+				
+				// Indicate failure
+				output.writeByte(1);
+				
+				// Logging
+				System.out.print("! Request FAIL ");
+				System.out.print(phoneNum);
+				System.out.println();
+				
+				return;
+				
+			}
+			
+			// Indicate success
+			output.writeByte(0);
+			
+			InetSocketAddress ip = info.ip;
+			int port = info.ip.getPort();
+			
+			output.write(ip.getAddress().getAddress());
+			output.writeShort(port);
+		
+			// Logging
+			System.out.print("? Phone info requested ");
+			System.out.print(phoneNum);
+			System.out.print(" ");
+			System.out.print(ip);
+			System.out.print(":");
+			System.out.print(port);
+			System.out.println();
+		}
+		
+		private void infoRemove(DataInputStream input, DataOutputStream output) throws IOException
+		{
+			long offeredKey = input.readLong();
+
+			for (Map.Entry<String, PhoneInfo> e :
+			phoneTable.entrySet()){
+				
+				if (e.getValue().sessionKey == offeredKey){
+					
+					phoneTable.remove(e.getKey());
+					output.writeByte(0); // Remove sucessful
+					return;
+					
+				}
+				
+			}
+			
+			output.writeByte(1); // Entry not in table or wrong key
+			
+			// Logging
+			System.out.println("! Remove requested for wrong key");
+		}
+		
+		private void infoUpdate(DataInputStream input, DataOutputStream output) throws IOException
+		{
+			long offeredKey = input.readLong();
+			
+			// Read phone number from socket
+			int phoneNumLength = input.readByte();
+			byte[] phoneNumBytes = new byte[phoneNumLength];
+			byte[] ipBytes = new byte[4];
+			input.readFully(phoneNumBytes);
+			String phoneNum = new String(phoneNumBytes);
+			input.readFully(ipBytes);
+			int port = input.readShort();
+			
+			// Get phone info from table
+			PhoneInfo info = phoneTable.get(phoneNum);
+			
+			if (offeredKey != info.sessionKey){
+				
+				output.writeByte(1); // Wrong key, failure
+				
+				// Logging
+				System.out.print("! Request FAIL ");
+				System.out.print(phoneNum);
+				System.out.println();
+				
+				return;
+			}
+			
+			info.ip = new InetSocketAddress(InetAddress.getByAddress(ipBytes), port);
+		}
+		
+		
 		public void run(){
 			
 			try{
@@ -88,158 +255,34 @@ public class SSLSwitchStation {
 				System.out.println();
 				
 				DataInputStream input = new DataInputStream(
-				connection.getInputStream());
+						connection.getInputStream());
 				DataOutputStream output = new DataOutputStream(
-				connection.getOutputStream());
+						connection.getOutputStream());
 
 				int requestType = input.readByte();
-
-				if (requestType == 1){ /* Register request */
-					
-					// Read phone number from socket
-					int phoneNumLength = input.readByte();
-					byte[] phoneNumBytes = new byte[phoneNumLength];
-					input.readFully(phoneNumBytes);
-					String phoneNum = new String(phoneNumBytes);
-					
-					// Read ip and port bytes from socket
-					int ip = input.readInt();
-					short port = input.readShort();
-					
-					long sessionKey = sessionKeyGen.nextLong();
-					
-					PhoneInfo info = new PhoneInfo(ip, port, sessionKey);
-					
-					if (phoneTable.containsValue(info)){
-						
-						output.writeByte(1);
-
-						// Logging
-						System.out.print("! Request FAIL ");
-						System.out.print(phoneNum);
-						System.out.println();
-						
-						return;
-						
-					}
-					
-					// Add value to table
-					phoneTable.put(phoneNum, info);
-					
-					// Indicate success
-					output.writeByte(0);
-					output.writeLong(sessionKey);
-					
-					// Logging
-					System.out.print("+ Phone info added ");
-					System.out.print(phoneNum);
-					System.out.print(',');
-					System.out.print(
-					Integer.toString((ip >> 32) & 0xff) +
-					Integer.toString((ip >> 16) & 0xff) +
-					Integer.toString((ip >> 8) & 0xff) +
-					Integer.toString(ip & 0xff));
-					System.out.print(':');
-					System.out.print(port);
-					System.out.println();
-					
-				} else if (requestType == 2){ /* Phone info request */
-					
-					// Read phone number from socket
-					int phoneNumLength = input.readByte();
-					byte[] phoneNumBytes = new byte[phoneNumLength];
-					input.readFully(phoneNumBytes);
-					String phoneNum = new String(phoneNumBytes);
-					
-					// Get phone info from table
-					PhoneInfo info = phoneTable.get(phoneNum);
-					
-					if (info == null){
-						
-						// Indicate failure
-						output.writeByte(1);
-						
-						// Logging
-						System.out.print("! Request FAIL ");
-						System.out.print(phoneNum);
-						System.out.println();
-						
-						return;
-						
-					}
-					
-					// Indicate success
-					output.writeByte(0);
-					
-					int ip = info.ip;
-					int port = info.port;
-					
-					output.writeInt(ip);
-					output.writeShort(port);
-				
-					// Logging
-					System.out.print("? Phone info requested ");
-					System.out.print(phoneNum);
-					System.out.print(" ");
-					System.out.print(ip);
-					System.out.print(":");
-					System.out.print(port);
-					System.out.println();
-						
-				} else if (requestType == 3){ /* Phone info remove */
-
-					long offeredKey = input.readLong();
-
-					for (Map.Entry<String, PhoneInfo> e :
-					phoneTable.entrySet()){
-						
-						if (e.getValue().sessionKey == offeredKey){
-							
-							phoneTable.remove(e.getKey());
-							output.writeByte(0); // Remove sucessful
-							return;
-							
-						}
-						
-					}
-					
-					output.writeByte(1); // Entry not in table or wrong key
-					
-					// Logging
-					System.out.println("! Remove requested for wrong key");
-					
-				} else if (requestType == 4){ /* Phone info update */
-
-					long offeredKey = input.readLong();
-					
-					// Read phone number from socket
-					int phoneNumLength = input.readByte();
-					byte[] phoneNumBytes = new byte[phoneNumLength];
-					input.readFully(phoneNumBytes);
-					String phoneNum = new String(phoneNumBytes);
-
-					int ip = input.readInt();
-					int port = input.readShort();
-					
-					// Get phone info from table
-					PhoneInfo info = phoneTable.get(phoneNum);
-					
-					if (offeredKey != info.sessionKey){
-						
-						output.writeByte(1); // Wrong key, failure
-						
-						// Logging
-						System.out.print("! Request FAIL ");
-						System.out.print(phoneNum);
-						System.out.println();
-						
-						return;
-					}
-					
-					info.ip = ip;
-					info.port = port;
-					
+				switch(requestType)
+				{
+					case 1:
+						System.out.println("Register request");
+						register(input, output);
+						break;
+					case 2:
+						System.out.println("Info request");
+						infoRequest(input, output);
+						break;
+					case 3:
+						System.out.println("Info remove request");
+						infoRemove(input, output);
+						break;
+					case 4:
+						System.out.println("Info update request");
+						infoUpdate(input, output);
+						break;
+					default:
+						System.out.println("Invalid request type: " + requestType);
+						break;
 				}
+				output.flush();
 			}catch (IOException ioe){
 				//abort communication
 			}catch (Exception e){
