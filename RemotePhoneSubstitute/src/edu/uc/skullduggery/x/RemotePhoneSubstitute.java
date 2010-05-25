e8445cd3acd9b2d45c6b58ca493a60387a3bd580
bakergo <bakergo@gmail.com>
2010-05-25 22:49:33 +0000

    Changed - Should work, but fails on connecting. I don't know why.
    git-svn-id: http://skullduggery.googlecode.com/svn/trunk@150 edacab16-c986-11de-9e96-3193d784f102

--- RemotePhoneSubstitute/src/edu/uc/skullduggery/x/RemotePhoneSubstitute.java
+++ RemotePhoneSubstitute/src/edu/uc/skullduggery/x/RemotePhoneSubstitute.java
@@ -3,10 +3,10 @@ package edu.uc.skullduggery.x;
 import java.io.*;
 import java.math.*;
 import java.net.*;
-import java.security.*;
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 
+import java.security.*;
 import java.security.spec.*;
 import java.security.interfaces.*;
 import java.util.*;
@@ -24,11 +24,28 @@ import java.util.*;
  */
 public class RemotePhoneSubstitute {
 
+	public enum MessageType
+	{
+		CALL,
+		PUBMOD,
+		PUBEXP,
+		SESKEY,
+		MESKEY,
+		ACCEPT,
+		REJECT,
+		BUSY,
+		VOICE,
+		HANGUP;
+	}
+
 	/* Input and output audio must be of this format */
 	private static final int sampleRate = 8000;     // 8khz
 	private static final int sampleSize = 16;       // 16-bit, 2-byte
 	private static final int channels = 1;          // mono
 	private static final boolean bigEndian = false; // little-endian
+	private static final byte[] magic = "SKUL".getBytes();
+	
+	
 	
 	/**
 	 * args[0] = phone to call's ip
@@ -39,63 +56,45 @@ public class RemotePhoneSubstitute {
 	 * @param args
 	 * @throws Exception
 	 */
+	
 	public static void main(String[] args) throws Exception{
 		
 		/*Prepare audio and logging*/
 		LoopedAudioInputStream lais =
-		new LoopedAudioInputStream(new File(args[2])); 
+		new LoopedAudioInputStream(new File(args[2]));
 		FileOutputStream alog = new FileOutputStream(args[3]);
 		
 		/*Open socket - call other phone*/
 		final String myPhoneNum = "1237890";
 		Socket callSocket = new Socket();
-		callSocket.connect(InetSocketAddress.createUnresolved(
-		args[0], Integer.parseInt(args[1])), 5000);
+		callSocket.connect(new InetSocketAddress(
+				InetAddress.getByName(args[0]),
+				Integer.parseInt(args[1])),
+				5000);
+
 		DataOutputStream dos =
 		new DataOutputStream(callSocket.getOutputStream());
 		DataInputStream dis =
 		new DataInputStream(callSocket.getInputStream());
 		
 		/*Initial contact*/
-		dos.write("SKUL".getBytes()); // MAGICBYTES
-		dos.writeByte(0); // CALL
-		dos.writeInt(myPhoneNum.length());
-		dos.write(myPhoneNum.getBytes());
-		readMagic(dis);
-		byte type = dis.readByte();
-		if (type == 7){ // BUSY
-			throw new IOException("Phone is busy");
-		}
-		if (type != 5){ // ACCEPT
-			throw new IOException("Rejected");
-		}
+		writeMessage(dos, MessageType.CALL, myPhoneNum.getBytes());
+		readMessage(dis);
 		
 		/*Key exchange handshake*/
 		KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-		kpg.initialize(4096);
+		kpg.initialize(1024);
 		
 		KeyPair localKeys = kpg.generateKeyPair();//changed code
 		RSAPublicKey pub = (RSAPublicKey) localKeys.getPublic();
 		BigInteger localPubMod = pub.getModulus();
 		BigInteger localPubExp = pub.getPublicExponent();
 		
-		dos.write("SKUL".getBytes());
-		dos.write(1); // PUBMOD
-		dos.writeInt(localPubMod.toByteArray().length);
-		dos.write(localPubMod.toByteArray());
-		dos.write("SKUL".getBytes());
-		dos.write(2); // PUBEXP
-		dos.writeInt(localPubExp.toByteArray().length);
-		dos.write(localPubExp.toByteArray());
+		writeMessage(dos, MessageType.PUBMOD, localPubMod.toByteArray());
+		writeMessage(dos, MessageType.PUBEXP, localPubExp.toByteArray());
+		byte[] remotePubModBytes = readMessage(dis, MessageType.PUBMOD);
+		byte[] remotePubExpBytes = readMessage(dis, MessageType.PUBEXP);
 		
-		readMagic(dis);
-		readType(dis, 1); // PUBMOD
-		byte[] remotePubModBytes = new byte[dis.readInt()];
-		dis.readFully(remotePubModBytes);
-		readMagic(dis);
-		readType(dis, 2); // PUBEXP
-		byte[] remotePubExpBytes = new byte[dis.readInt()];
-		dis.readFully(remotePubExpBytes);
 		BigInteger remotePubMod = new BigInteger(remotePubModBytes);
 		BigInteger remotePubExp = new BigInteger(remotePubExpBytes);
 		
@@ -105,25 +104,25 @@ public class RemotePhoneSubstitute {
 		PublicKey remotePublicKey = keygen.generatePublic(remotePubKeySpec);
 		
 		/*Get session key*/
-		Cipher decryptor = Cipher.getInstance(
-				localKeys.getPrivate().getAlgorithm());
-				decryptor.init(Cipher.DECRYPT_MODE, localKeys.getPrivate());
-		dis = new DataInputStream(new CipherInputStream(
-		callSocket.getInputStream(), decryptor));
+		Cipher decryptor = 
+		Cipher.getInstance(localKeys.getPrivate().getAlgorithm());
+		decryptor.init(Cipher.DECRYPT_MODE, localKeys.getPrivate());
+				
+		System.out.println("Reading session key");
+		byte[] sessionKeyBytes = readMessage(dis, MessageType.SESKEY);
+		System.out.println("Session key read");
 		
-		readMagic(dis);
-		readType(dis, 3); // SESKEY
-		int sessionKeyLength = dis.readInt();
-		byte[] sessionKeyBytes =
-		new byte[sessionKeyLength > 0 ? sessionKeyLength : 128];
-		SecretKey sessionKey = new SecretKeySpec(sessionKeyBytes, "AES");
+		System.out.println("Session Key:" + sessionKeyBytes.length);
+		
+		byte[] sessionKeyBytesDec = decryptor.doFinal(sessionKeyBytes);
+				
+		SecretKey sessionKey = new SecretKeySpec(sessionKeyBytesDec, "AES");
 		
 		/*Start talking*/
-		Cipher encryptor = Cipher.getInstance(
-		remotePublicKey.getAlgorithm());
+		Cipher encryptor = Cipher.getInstance(sessionKey.getAlgorithm());
 		encryptor.init(Cipher.ENCRYPT_MODE, sessionKey);
-		decryptor = Cipher.getInstance(
-		localKeys.getPrivate().getAlgorithm());
+		
+		decryptor = Cipher.getInstance(sessionKey.getAlgorithm());
 		decryptor.init(Cipher.DECRYPT_MODE, sessionKey);
 
 		dos = new DataOutputStream(new CipherOutputStream(
@@ -133,30 +132,27 @@ public class RemotePhoneSubstitute {
 		
 		byte[] buf = new byte[1024 * 4];
 		int bytesRead = 0;
+		int sentPackets = 0;
 		
 		while (true){
+			System.out.println("Reading from the audio stream");
 			// Read from loop (similar to microphone record)
 			bytesRead = lais.read(buf, 0, buf.length);
+			System.out.println("Read " + bytesRead + " bytes");
+			System.out.println("Writing data to output stream");
 			
 			// Write packet of voice data
-			dos.write("SKUL".getBytes());
-			dos.writeByte(8); // VOICE
-			dos.writeInt(bytesRead);
-			dos.write(buf, 0, bytesRead);
+			writeMessage(dos, MessageType.VOICE,buf, bytesRead);
+			System.out.println("Data written to output stream");
 			
-			// Read packet of voice data
-			readMagic(dis);
-			int messageType = dis.readByte();
-			if (messageType == 9){ // HANGUP
-				throw new EOFException("Remote phone hung-up");
-			} else if (messageType != 8){ // VOICE
-				throw new IOException("Unexpected packet type");
-			}
-			bytesRead = dis.readInt();
-			dis.readFully(buf, 0, bytesRead);
+//			if(sentPackets > 1){
+//				System.out.println("Reading incoming message");
+//				buf = readMessage(dis);
+//			}
 			
 			// Write to audio log (similar to speaker play)
-			alog.write(buf, 0, bytesRead);
+			System.out.println("Writing incoming message to audio log");
+			alog.write(buf);
 		}
 	}
 	
@@ -167,13 +163,51 @@ public class RemotePhoneSubstitute {
 			throw new IOException("Wrong magic bytes");
 	}
 
-	private static void readType(DataInputStream dis, int expectedType)
+	private static void readType(DataInputStream dis, MessageType expectedType)
 	throws IOException{
-		if (dis.readByte() != expectedType){
+		if (dis.readByte() != (byte) expectedType.ordinal()){
 			throw new IOException("Unexpected packet type");
 		}
 	}
 	
+	private static byte[] readMessage(DataInputStream dis, MessageType type) throws IOException
+	{
+		readMagic(dis);
+		readType(dis, type);
+		int len = dis.readInt();
+		if(len <= 0) return null;
+		byte[] data = new byte[len];
+		dis.readFully(data);
+		return data;		
+	}
+	
+	private static byte[] readMessage(DataInputStream dis) throws IOException
+	{
+		readMagic(dis);
+		dis.readByte();
+		int len = dis.readInt();
+		if(len <= 0) return null;
+		byte[] data = new byte[len];
+		dis.readFully(data);
+		return data;		
+	}
+	
+	private static void writeMessage(DataOutputStream dos,MessageType type, byte[] data) throws IOException
+	{
+		dos.write(magic);
+		dos.write((byte) type.ordinal());
+		dos.writeInt(data.length);
+		dos.write(data);		
+	}
+	
+	private static void writeMessage(DataOutputStream dos,MessageType type, byte[] data, int len) throws IOException
+	{
+		dos.write(magic);
+		dos.write((byte) type.ordinal());
+		dos.writeInt(len);
+		dos.write(data,0,len);		
+	}
+
 	private static class LoopedAudioInputStream{
 		RandomAccessFile raf;
 		public LoopedAudioInputStream(File f) throws Exception{
@@ -188,5 +222,4 @@ public class RemotePhoneSubstitute {
 			return bytesRead;
 		}
 	}
-	
 }
